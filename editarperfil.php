f0232d1be66c6371e05185c61ee3d1e6b1e84859
ignacioriffo <ignacio.riffop@sansano.usm.cl>
2020-08-02 19:11:11 -0400

    correccion pequeña de container

--- editarperfil.php
+++ editarperfil.php
@@ -97,17 +97,17 @@ if(isset($_POST['password'])){
         echo '<textarea class="form-control" name="newbiografia" id="newbiografia" rows="3"></textarea>';
         echo '</div>';
         echo '<button type="submit" class="btn btn-primary">Enviar</button>';
-        echo '</div>';
+        
         echo '</form>';
       }
 
-
+      
       echo "<form action='editarperfil.php'  method='post'>";
       echo "<button type='submit' class='btn btn-link' name='borrarcuenta' value='" . $user->getId() . "'>Borrar cuenta</button>";
       echo "</form>";
     ?>
 
-
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
